971c618c19d73335e9d2d91786c9c172e341ac81
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-12-13 13:13:38 +0530

    added Auth-middlware and JOI validation to middleware

---
+++ Backend/middleware/validate.js
@@ -0,0 +1,17 @@
+const { celebrate, Joi, Segments } = require("celebrate");
+
+exports.validateSignup = celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().min(6).required(),
+        role: Joi.string().valid("user", "admin")
+    })
+});
+
+exports.validateLogin = celebrate({
+    [Segments.BODY]: Joi.object().keys({
+        email: Joi.string().email().required(),
+        password: Joi.string().required()
+    })
+});
