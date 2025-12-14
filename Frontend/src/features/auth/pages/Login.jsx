da21498787a1aec7b8d405f5f2bd4056e39c3e9f
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-12-14 12:06:26 +0530

    added basic code for login

---
+++ Frontend/src/features/auth/pages/Login.jsx
@@ -0,0 +1,21 @@
+import { useForm } from "react-hook-form"
+import api from "@/lib/axios"
+import { useAuth } from "@/context/AuthContext" 
+
+export default function Login() {
+  const { register, handleSubmit } = useForm()
+  const { login } = useAuth()
+
+  const onSubmit = async (data) => {
+    const res = await api.post("/auth/login", data)
+    login(res.data)
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input {...register("email")} />
+      <input {...register("password")} type="password" />
+      <button>Login</button>
+    </form>
+  )
+}
