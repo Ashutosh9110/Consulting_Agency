95fa73298e5f24d709adbfa3fc61a497a6cc2571
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-12-14 07:50:46 +0530

    added  Google Login routes

--- Backend/routes/authRoutes.js
+++ Backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { signup, login, refreshToken } = require("../controllers/authController");
+const passport = require("passport");
+const { signup, login, refreshToken, googleAuthCallback } = require("../controllers/authController");
 const { validateSignup, validateLogin } = require("../middleware/validate");
 const { auth, authorizeRoles } = require("../middleware/authMiddleware");
 
@@ -10,7 +11,8 @@ router.post("/refresh-token", refreshToken);
 router.get("/admin-only", auth, authorizeRoles("admin"), (req, res) => {
     res.json({ message: "Admin Access Granted" });
 });
-
+router.get( "/google", passport.authenticate("google", { scope: ["profile", "email"] }))
+router.get( "/google/callback", passport.authenticate("google", { session: false }), googleAuthCallback)
 
 
 
