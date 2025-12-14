e51b9c75781772536d8848e7e3722a241f012da4
Ashutosh Adhikari <veni_vidi.vici@outlook.com>
2025-12-14 12:02:27 +0530

    added basic authContext logic

---
+++ Frontend/src/context/AuthContext.jsx
@@ -0,0 +1,28 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext(null);
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const login = ({ accessToken, user }) => {
+    localStorage.setItem("accessToken", accessToken);
+    setUser(user);
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    setUser(null);
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ user, isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
